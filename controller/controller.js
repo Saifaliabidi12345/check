1943c1bd7fafa8a95853954ac96be11ed35064a8
deepakdeakin <s220007878@deakin.edu.au>
2023-08-21 12:17:41 +1000

    Committing for Practical 5

---
+++ controller/controller.js
@@ -0,0 +1,26 @@
+
+const collection = require('../Models/cat');
+
+const postCat = (req, res) => {
+    let cat = req.body;
+    collection.postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({ statusCode: 201, data: result, message: 'success' });
+        }
+    });
+};
+
+const getAllCats = (req, res) => {
+    collection.getAllCats((err, result) => {
+        if (!err) {
+            res.json({ statusCode: 200, data: result, message: 'get all cats successful' });
+        }
+    });
+};
+
+module.exports = {
+    postCat: postCat,
+    getAllCats: getAllCats
+};
+
+
