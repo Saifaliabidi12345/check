e12b04a8706ad90b2bc0ddee1ca0bdd804da363e
deepakdeakin <s220007878@deakin.edu.au>
2023-08-21 12:16:42 +1000

    Committing for Practical 5

---
+++ dbConnection.js
@@ -0,0 +1,16 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.kmm9t86.mongodb.net/?retryWrites=true&w=majority";
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+client.connect();
+
+module.exports = client;
