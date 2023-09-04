333600fff32531c949d26f9826de6e7ba92593d0
deepakdeakin <s220007878@deakin.edu.au>
2023-09-04 16:30:14 +1000

    Committing For Socket Practical 7

--- server.js
+++ server.js
@@ -5,13 +5,30 @@ require('./dbConnection');
 let router = require('./router/router');
 let port = process.env.port || 3000;
 
+let http = require ('http').createServer(app);
+let io = require ('socket.io')(http);
+const {Socket} = require('socket.io');
+
+
+
 app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api/cat',router)
 
+io.on('connection',(socket)=>{
+    console.log('User has Connected');
+    socket.on('disconnect', () => {
+        console.log('User has disconnected');
+    });
 
-app.listen(port, ()=>{
+    setInterval(()=>{
+        socket.emit('number', parseInt(Math.random()*10));
+    }, 1000)
+});
+
+
+http.listen(port, ()=>{
     console.log('express server started');
 });
 
